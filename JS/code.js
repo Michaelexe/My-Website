e563a539e0749277bc3f16fdf3e1c4d27c8cceed
Michaelexe <michaelispahani@gmail.com>
2021-08-10 22:46:49 +0430

    Changes Code.html completely with new css and js

---
+++ JS/code.js
@@ -0,0 +1,112 @@
+function coronaLiveClick() {
+	document.getElementById('coronalive-tab').style.borderBottom = '4px solid rgb(255,0,0)';
+	document.getElementById('snake-game-tab').style.borderBottom = '4px solid white';
+	document.getElementById('tik-tak-toe-tab').style.borderBottom = '4px solid white';
+	document.getElementById('password-improver-tab').style.borderBottom = '4px solid white';
+	document.getElementById('this-website-tab').style.borderBottom = '4px solid white';
+
+	document.getElementById('coronalive-tab-header').style.color = 'rgb(255,0,0)';
+	document.getElementById('password-improver-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('this-website-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('tik-tak-toe-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('snake-game-tab-header').style.color = 'rgb(180,180,180)';
+
+	document.getElementById('coronalive-content').style.display = 'block';
+	document.getElementById('snake-game-content').style.display = 'none';
+	document.getElementById('website-content').style.display = 'none';
+	document.getElementById('tik-tak-toe-content').style.display = 'none';
+	document.getElementById('password-improver-content').style.display = 'none';
+}
+
+
+
+function snakeGameClick() {
+	document.getElementById('coronalive-tab').style.borderBottom = '4px solid white';
+	document.getElementById('snake-game-tab').style.borderBottom = '4px solid rgb(255,255,0)';
+	document.getElementById('tik-tak-toe-tab').style.borderBottom = '4px solid white';
+	document.getElementById('password-improver-tab').style.borderBottom = '4px solid white';
+	document.getElementById('this-website-tab').style.borderBottom = '4px solid white';
+
+	document.getElementById('coronalive-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('password-improver-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('this-website-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('tik-tak-toe-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('snake-game-tab-header').style.color = 'rgb(255,255,0)';
+
+	document.getElementById('snake-game-content').style.display = 'block';
+	document.getElementById('coronalive-content').style.display = 'none';
+	document.getElementById('website-content').style.display = 'none';
+	document.getElementById('tik-tak-toe-content').style.display = 'none';
+	document.getElementById('password-improver-content').style.display = 'none';
+}
+
+
+
+
+function websiteClick() {
+	document.getElementById('coronalive-tab').style.borderBottom = '4px solid white';
+	document.getElementById('snake-game-tab').style.borderBottom = '4px solid white';
+	document.getElementById('tik-tak-toe-tab').style.borderBottom = '4px solid white';
+	document.getElementById('password-improver-tab').style.borderBottom = '4px solid white';
+	document.getElementById('this-website-tab').style.borderBottom = '4px solid rgb(50,0,255)';
+
+	document.getElementById('coronalive-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('password-improver-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('this-website-tab-header').style.color = 'rgb(50,0,255)';
+	document.getElementById('tik-tak-toe-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('snake-game-tab-header').style.color = 'rgb(180,180,180)';
+
+	document.getElementById('snake-game-content').style.display = 'none';
+	document.getElementById('coronalive-content').style.display = 'none';
+	document.getElementById('website-content').style.display = 'block';
+	document.getElementById('tik-tak-toe-content').style.display = 'none';
+	document.getElementById('password-improver-content').style.display = 'none';
+
+}
+
+
+
+function tikTakToeClick() {
+	document.getElementById('coronalive-tab').style.borderBottom = '4px solid white';
+	document.getElementById('snake-game-tab').style.borderBottom = '4px solid white';
+	document.getElementById('tik-tak-toe-tab').style.borderBottom = '4px solid rgb(0,255,0)';
+	document.getElementById('password-improver-tab').style.borderBottom = '4px solid white';
+	document.getElementById('this-website-tab').style.borderBottom = '4px solid white';
+
+	document.getElementById('coronalive-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('password-improver-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('this-website-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('tik-tak-toe-tab-header').style.color = 'rgb(0,255,0)';
+	document.getElementById('snake-game-tab-header').style.color = 'rgb(180,180,180)';
+
+	document.getElementById('snake-game-content').style.display = 'none';
+	document.getElementById('coronalive-content').style.display = 'none';
+	document.getElementById('website-content').style.display = 'none';
+	document.getElementById('tik-tak-toe-content').style.display = 'block';
+	document.getElementById('password-improver-content').style.display = 'none';
+
+}
+
+
+
+function passwordImproverClick() {
+	document.getElementById('coronalive-tab').style.borderBottom = '4px solid white';
+	document.getElementById('snake-game-tab').style.borderBottom = '4px solid white';
+	document.getElementById('tik-tak-toe-tab').style.borderBottom = '4px solid white';
+	document.getElementById('password-improver-tab').style.borderBottom = '4px solid rgb(0,150,255)';
+	document.getElementById('this-website-tab').style.borderBottom = '4px solid white';
+
+	document.getElementById('coronalive-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('password-improver-tab-header').style.color = 'rgb(0,150,255)';
+	document.getElementById('this-website-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('tik-tak-toe-tab-header').style.color = 'rgb(180,180,180)';
+	document.getElementById('snake-game-tab-header').style.color = 'rgb(180,180,180)';
+
+	document.getElementById('snake-game-content').style.display = 'none';
+	document.getElementById('coronalive-content').style.display = 'none';
+	document.getElementById('website-content').style.display = 'none';
+	document.getElementById('tik-tak-toe-content').style.display = 'none';
+	document.getElementById('password-improver-content').style.display = 'block';
+
+
+}
